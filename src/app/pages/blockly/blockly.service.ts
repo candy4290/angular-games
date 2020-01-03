e64eebb501e70391fcef284ce2f5f290bc06d42b
candy4390 <2459402883@qq.com>
2020-01-03 10:34:03 +0800

    考虑键值对

--- src/app/pages/blockly/blockly.service.ts
+++ src/app/pages/blockly/blockly.service.ts
@@ -404,7 +404,8 @@ export class BlocklyService {
       values.forEach(value => {
         if (value.block[0].$.type === 'logic_compare') {
           result.push({
-            name: (value.block[0].value || [])[1] ? value.block[0].value[1].block[0].field[0]._ : '',
+            name: (value.block[0].value || [])[0] ? value.block[0].value[0].block[0].field[0]._ : '',
+            value: (value.block[0].value || [])[1] ? value.block[0].value[1].block[0].field[0]._ : '',
             operator: 'SINGLE',
             type: 'TAG',
             subs: []
